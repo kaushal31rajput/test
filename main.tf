0f258bb5d60249045813f7bf8e3c95a12eed3b31
kaushal31rajput <kaushal31rajput@gmail.com>
2022-10-09 14:35:34 +0530

    instance

--- main.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_instance" "prod_ec2" {
-  ami        =   "ami-026b57f3c383c2eec"
-  instance_type = "t2.micro"
-}
-
--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
-#jnjckn
 provider "aws" {
- region     = "us-east-1"
+region = "us-east-1"
 }
-
