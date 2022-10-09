ed52daa07627e9e3c5d9fb6217d2486d888363d2
kaushal31rajput <kaushal31rajput@gmail.com>
2022-10-09 14:42:33 +0530

    new ec2

--- ec2.tf
+++ ec2.tf
@@ -2,3 +2,9 @@ resource "aws_instance" "web" {
   ami           = "ami-026b57f3c383c2eec"
   instance_type = "t3.micro"
 }
+
+resource "aws_instance" "web" {
+  ami           = "ami-026b57f3c383c2eec"
+  instance_type = "t2.micro"
+}
+
