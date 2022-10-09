dbab0a90dd1323d58e1edee3892b79ca00767a3a
kaushal31rajput <kaushal31rajput@gmail.com>
2022-10-09 14:43:45 +0530

    bew ec2

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web1" {
   ami           = "ami-026b57f3c383c2eec"
   instance_type = "t2.micro"
 }
